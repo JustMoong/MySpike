967f3228ed438434dbc28f2efc32e2d1cb39ff6c
Justmoong <andudyun0504@gmail.com>
2024-03-10 16:03:45 +0900

    First Commit

--- MySpike/ContentView.swift
+++ MySpike/ContentView.swift
@@ -10,15 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                RecentTrackingView()
+            }
+            VStack {
+                
+            }
         }
-        .padding()
     }
-}
-
-#Preview {
-    ContentView()
+    
 }
---
+++ MySpike/RecentTrackingView.swift
@@ -0,0 +1,19 @@
+//
+//  RecentTrackingView.swift
+//  MySpike
+//
+//  Created by 윤무영 on 3/10/24.
+//
+
+import SwiftUI
+
+struct RecentTrackingView: View {
+    var body: some View {
+        Text("Today, March 10th")
+            .multilineTextAlignment(.leading)
+    }
+}
+
+#Preview {
+    RecentTrackingView()
+}
