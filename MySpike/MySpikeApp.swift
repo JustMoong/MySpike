ccdfaea6b37e63e8c8aaf2c2c9c6a60e724d919c
Justmoong <andudyun0504@gmail.com>
2024-03-10 15:54:25 +0900

    Initial Commit

---
+++ MySpike/MySpikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySpikeApp.swift
+//  MySpike
+//
+//  Created by 윤무영 on 3/10/24.
+//
+
+import SwiftUI
+
+@main
+struct MySpikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
